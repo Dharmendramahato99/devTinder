a7a5024973e4bdeb6c3c3b944a55302a28da27ea
Dharmendra <mahatodharmendra726@gmail.com>
2025-01-28 16:58:24 +0530

    dynamic data in signupAPI

--- src/app.js
+++ src/app.js
@@ -7,22 +7,34 @@ const app = express();
 
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
+     console.log(req.body);
+     const user = new User(req.body);
+        try{
+            await user.save();
+            res.send("User Added successfully!");
+        } catch (err) {
+            res.status(400).send("Error saving the user:" + err.message);
+        }
+    
+
     // Creating a new instance of the User model
 
-    const user = new User({
-        firstName: "Dharmendra",
-        lastName: "Mahato",
-        emailId: "mahatodharmendra726@gmail.com",
-        password: "Dhar@123",
-    });
+    // const user = new User({
+    //     firstName: "Dharmendra",
+    //     lastName: "Mahato",
+    //     emailId: "mahatodharmendra726@gmail.com",
+    //     password: "Dhar@123",
+    // });
 
-    try{
-        await user.save();
-        res.send("User Added successfully!");
-    } catch (err) {
-        res.status(400).send("Error saving the user:" + err.message);
-    }
+    // try{
+    //     await user.save();
+    //     res.send("User Added successfully!");
+    // } catch (err) {
+    //     res.status(400).send("Error saving the user:" + err.message);
+    // }
 
    
 });
