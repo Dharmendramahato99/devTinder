4bb3c39a18e7acdcc70af23abbb050c6f0e64256
Dharmendra <mahatodharmendra726@gmail.com>
2025-01-29 15:38:10 +0530

    schema Validation

--- src/app.js
+++ src/app.js
@@ -1,16 +1,11 @@
 const express = require('express');
-
 const connectDB = require("./config/database");
-
 const app = express();
-
-
 const User = require("./models/user");
-
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-     console.log(req.body);
+     //console.log(req.body);
      const user = new User(req.body);
         try{
             await user.save();
@@ -37,6 +32,62 @@ app.post("/signup", async (req, res) => {
     // }
 
    
+});
+
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async(req,res) => {
+        try{
+            const users = await User.find({});
+            res.send(users);
+        }
+        catch(err){
+            res.status(400).send("Something went wrong");
+        }
+    });
+  
+
+//Get user by email
+app.get("/user", async(req,res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const users = await User.find({emailId:userEmail});
+        if(users.length === 0){
+            res.status(404).send("User not found");
+        }else{
+            res.send(users);
+        }
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
+});
+
+// Delete a user from the database
+app.delete("/user", async(req,res) => {
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete({_id:userId});
+        res.send("User deleted successfully");
+    } catch(err){
+        res.status(400).send("Something went wrong");
+    }
+});
+
+// Update data of the user
+app.patch("/user",async(req,res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+        const user = await User.findByIdAndUpdate({_id: userId},data,{
+        runValidators: true,
+        });
+        res.send("User updated successfully");
+    }
+    catch(err){
+        res.status(400).send("UPDATE FAiLED:" + err.message);
+    }
 });
 
 connectDB().then(() => {
